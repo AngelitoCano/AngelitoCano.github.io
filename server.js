659efdc5ba90f061a7aabe23d49d2cd6aea4b6bf
Giova2001 <106548512+Giova2001@users.noreply.github.com>
2024-06-25 22:52:40 -0600

    Update server.js
    prueba dos

--- server.js
+++ server.js
@@ -2,15 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Servir archivos estáticos desde el directorio de construcción
 app.use(express.static(path.join(__dirname, 'build')));
 
-// Enviar index.html para todas las solicitudes no manejadas por archivos estáticos
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-// Configurar el puerto
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
